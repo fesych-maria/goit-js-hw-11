ac2df69d09712f0d3ce491eae21009d7ae1902bb
maria <fesychmarimia@gmail.com>
2025-02-19 09:16:29 +0100

    add styles

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -5,19 +5,21 @@ import { refs } from '../main.js';
 
 function imgTemplate(img) {
   return `<li class="gallery-item">
+          <div class="img-wrapper">
           <a href="${img.largeImageURL}" class="gallery-link">
-          <img src="${img.webformatURL}" alt="${img.tags}" class="" />
+          <img src="${img.webformatURL}" alt="${img.tags}" class="gallery-img" />
           </a>
+          </div>
           <ul class="img-text-list">
-            <li class="img-text-item">
+            <li class="img-text-item first-category">
               <p class="img-text-category">Likes</p>
               <p class="img-text-value">${img.likes}</p>
             </li>
-            <li class="img-text-item">
+            <li class="img-text-item second-category">
               <p class="img-text-category">Views</p>
               <p class="img-text-value">${img.views}</p>
             </li>
-            <li class="img-text-item">
+            <li class="img-text-item third-category">
               <p class="img-text-category">Comments</p>
               <p class="img-text-value">${img.comments}</p>
             </li>
